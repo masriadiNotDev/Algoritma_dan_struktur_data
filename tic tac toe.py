6de962dd77ef616d05c7b8c692dbbc8831109648
masriadiNotDev <adiganzxd@gmail.com>
2025-11-27 17:42:31 +0800

    Enhance Tic Tac Toe UI and add restart/exit features
    Updated the Tic Tac Toe game with a modified UI and added functionality for game restart and exit buttons. Enhanced message prompts for game outcomes and improved button configurations.

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,9 +1,19 @@
+#tictactoe 
+#MASRIADI SISTEM INFORMASI
+#hasiyl modifikasi dari program tictactoe dasar
+#tambahan popup messagebox dari tkinter dan tambahan tombol untuk mengakhiri program
+#perubahan warna dan posisi tombol
+
+
 
 from tkinter import *
+from tkinter import messagebox #untuk menampilkan pesan atau popup
 import random
 window = Tk()
 x_state = []   
 o_state = []
+
+window.title("Welcome to TICTACTOE game")
 
 #creating a turn variable
 turn = 0
@@ -15,19 +25,21 @@ winning_list = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 # membuat jendela mengambang menggunakan module tkinter
 window.title("TTC mod by masriadi")
-window.geometry("600x500")
+window.configure(bg="blue")
+window.geometry("625x600")
 #adding padding to the window
-window.config(padx=100, pady=40)
+window.config(padx=10, pady=10)
 
 #label dan font
 title_label = Label(text="klik mulai untuk memulai", font=("arial", 20, "bold"))
-title_label.grid(row=0, column=1, columnspan=3)
+title_label.grid(row=4, column=1, padx=15, pady=10)
 
 #creating a game_tied function  
 def game_tied():
     global x_state, o_state
     if x_state.count(1) + o_state.count(1) == 9: #count 1's in x_state and o_state
         title_label.config(text="seri")
+        messagebox.showinfo("permainan seri", "Permainan berakhir dengan seri!") #untuk popup pesan seri
         for i in button_list:
             i.config(command=DISABLED)
 
@@ -45,13 +57,12 @@ def create_cells():
                 font=("arial", 30, "bold"), 
                 width=8, 
                 height=3, 
-                borderwidth=2,
-                bg ='black',
+                borderwidth=1,
+                bg ='darkblue',
                 state=DISABLED,
             )
             button_list.append(cell)
             cell.grid(row=i+1, column=j+1)
-
 #function to print the board
 def printboard(x_state, o_state):     
     #function to check game tied
@@ -63,43 +74,44 @@ def printboard(x_state, o_state):
         for j in range(3):
             if x_state[i*3+j] == 1:
                 button_list[i*3+j].config(text="X",
-                                            fg="red",
+                                            fg="white",
                                             font=("arial", 30, "bold"), 
                                             command=DISABLED)
             elif o_state[i*3+j] == 1:
                 button_list[i*3+j].config(text="O",
-                                            fg="blue",
+                                            fg="cyan",
                                             font=("arial", 30, "bold"), 
                                             command=DISABLED)
             
             else:
                 button_list[i*3+j].config(text=" ")
 
+def restart_game():
+    global turn, x_state, o_state, button_list
+    turn = random.randint(0, 1)
+    x_state = [0,0,0,0,0,0,0,0,0]
+    o_state = [0,0,0,0,0,0,0,0,0]
+
+#enabling the buttons
+    for i in range(3):
+        for j in range(3):
+            button_list[i*3+j].config( state=NORMAL,command=lambda i=i, j=j: handle_click(i, j))
+
+    printboard(x_state, o_state)
+
+
+
 
 #ngecek siapa yg mnang 
-def check_winner(x_state, o_state):
-    for i in winning_list:
-        if x_state[i[0]] and x_state[i[1]] and x_state[i[2]]:
-            title_label.config(text="X menang")
-            printboard(x_state, o_state)
-
-            return 1
-        if o_state[i[0]] and o_state[i[1]] and o_state[i[2]]:
-            title_label.config(text="O menang")
-            printboard(x_state, o_state)
-            printboard(x_state, o_state)
-            return 0
-        
-    return -1
 
 #function to handle the button click
 def handle_click(row, col):
     global turn, x_state, o_state
     if turn:
-        title_label.config(text="giliran O")
+        title_label.config(text="GILIRAN O",fg="lightblue", bg="blue" )
         x_state[row*3+col] = 1
     else:
-        title_label.config(text="giliran X")
+        title_label.config(text="GILIRAN X",fg="lightblue", bg="blue" )
         o_state[row*3+col] = 1
     printboard(x_state, o_state)
     result = check_winner(x_state, o_state)
@@ -113,8 +125,11 @@ def handle_click(row, col):
 
 
 # 1. start button
-start_button = Button(text="mulai", font=("arial", 16, "bold"))
-start_button.grid(row=4, column=2, padx=15, pady=10)
+start_button = Button(text="MULAI", font=("arial", 16, "bold"),  fg="white", bg="darkblue" )
+start_button.grid(row=4, column=2, padx=15, pady=10, ipady=10 , ipadx=5)
+#akhiri button 
+akhiri_button = Button(text="KELUAR", font=("arial", 16, "bold"), fg="white", bg="darkblue"  )
+akhiri_button.grid(row=4, column=3, padx=12, pady=4 , ipady=10 ,)
 
 #function to start the game
 def start_game():
@@ -129,14 +144,43 @@ def start_game():
             button_list[i*3+j].config( state=NORMAL,command=lambda i=i, j=j: handle_click(i, j))
 
     printboard(x_state, o_state)
-    title_label.config(text="giliran O" if turn == 0 else "giliran X")
+    title_label.config(text="GILIRAN O" if turn == 0 else "GILIRAN X",fg="white", bg="blue" )
 
 #binding the start button to the start_game function
-    start_button.config(text="mulai ulang  ")
+    start_button.config(text="RESTART")
+    akhiri_button.config(text="AKHIRI")
 
 #binding the start button to the start_game function
-start_button.config(command=start_game)    
+start_button.config(command=start_game)  
+akhiri_button.config(command=quit)
 # calling the create_cells function
+def check_winner(x_state, o_state):
+    for i in winning_list:
+        if x_state[i[0]] and x_state[i[1]] and x_state[i[2]]:
+            title_label.config(text="X MENANG")
+            jawab = messagebox.askyesno("X menang", "Lanjutkan permainan?")  # POPUP
+            if jawab:
+              print("1")
+            else:
+                quit()
+            printboard(x_state, o_state)
+
+            return 1
+        if o_state[i[0]] and o_state[i[1]] and o_state[i[2]]:
+            title_label.config(text="O menang")
+            jawab = messagebox.askyesno("O MENANG", "Lanjutkan permainan?") # POPUP
+            if jawab:
+              print("restart")
+              start_game()
+            else:
+                quit()
+            printboard(x_state, o_state)
+            printboard(x_state, o_state)
+            return 0
+        
+    return -1
+
 create_cells()
+start_game()
 #creating mainloop for the window
 window.mainloop()
